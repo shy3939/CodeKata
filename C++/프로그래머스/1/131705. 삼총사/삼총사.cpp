098806f60a07212fb57515f5e8d3d3f1baee9beb
shy <166278050+shy3939@users.noreply.github.com>
2026-01-20 10:06:22 +0900

    Update 삼총사.cpp

--- C++/프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ C++/프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -4,23 +4,7 @@
 
 using namespace std;
 
-long long factorial(int number) {
-    long long result = 1;
-    for(int i = 1; i <= number; i++) {
-        result *= i;
-    }
-    return result;
-}
-
 int solution(vector<int> number) {
-    int answer = 0;
-    int size = number.size();
-    int fac = size-3;
-    long long a = factorial(size);
-    long long b = factorial(3);
-    long long c = factorial(fac);
-    int x = a/(b*c);
-    // answer = x;
     
     for(int i = 0; i < size; i++) {
         for(int j = 1 + i; j < size; j++) {
