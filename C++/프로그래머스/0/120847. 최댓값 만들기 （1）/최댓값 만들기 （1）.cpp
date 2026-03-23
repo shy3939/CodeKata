718ec4ed7139536428ce4121aa90cb72ff1650f0
shy <kpky33@naver.com>
2026-03-23 09:31:14 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    int size = numbers.size();
+    sort(numbers.begin(), numbers.end());
+    
+    answer = numbers[size-1] * numbers[size-2];
+    
+    return answer;
+}
