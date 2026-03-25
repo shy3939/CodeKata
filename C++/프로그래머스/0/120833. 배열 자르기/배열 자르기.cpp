280909a1ab32f5882bc3ac09ddfbe40f8d7a9f1d
shy <kpky33@naver.com>
2026-03-25 09:13:31 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120833. 배열 자르기/배열 자르기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, int num1, int num2) {
+    int result = num2 - num1 + 1;
+    int count = 0;
+    vector<int> answer(result, 0);
+    int size = numbers.size();
+    
+    for(int i = 0; i < size; i++) {
+        if(i >= num1 && i <= num2) {
+            answer[count] = numbers[i];
+            count++;
+        }
+    }
+    
+    return answer;
+}
