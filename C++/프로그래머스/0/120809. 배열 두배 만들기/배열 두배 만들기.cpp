d7ff6d448591481e41ebcb27aa2feb540f37e87d
shy <kpky33@naver.com>
2026-03-26 09:14:15 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.20 ms, Memory: 4.22 MB -BaekjoonHub

--- C++/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
+++ C++/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -6,11 +6,10 @@ using namespace std;
 vector<int> solution(vector<int> numbers) {
 
     int size = numbers.size();
-    vector<int> answer(size, 0);
     
     for(int i = 0; i < size; i++) {
-        answer[i] = numbers[i] * 2;
+        numbers[i] = numbers[i] << 1;
     }
     
-    return answer;
+    return numbers;
 }
